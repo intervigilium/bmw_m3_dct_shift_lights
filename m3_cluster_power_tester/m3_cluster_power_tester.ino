fbbd0f20496c26b8d006e19bd54c9fd13e30b8ba
Ethan Chen <intervigil@gmail.com>
2021-04-03 20:38:04 -0700

    power_tester: Light all LEDs up to index

--- m3_cluster_power_tester/m3_cluster_power_tester.ino
+++ m3_cluster_power_tester/m3_cluster_power_tester.ino
@@ -1,15 +1,40 @@
-int green_led = 7;
+int internalLed = 13;
+
+const int kNumLeds = 8;
+
+int shiftSerialIn = 2;
+int shiftClock = 3;
+int shiftRegClock = 4;
+int shiftClear = 19;
+int shiftOutputEnable = 18;
+
+void enableLightsUpTo(int idx) {
+  digitalWrite(shiftRegClock, LOW);
+  shiftOut(shiftSerialIn, shiftClock, MSBFIRST, (1 << idx) - 1);
+  digitalWrite(shiftRegClock, HIGH);
+}
 
 // the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(green_led, OUTPUT);     
+void setup() {
+  pinMode(shiftSerialIn, OUTPUT);
+  pinMode(shiftClock, OUTPUT);
+  pinMode(shiftRegClock, OUTPUT);
+  pinMode(shiftClear, OUTPUT);
+  pinMode(shiftOutputEnable, OUTPUT);
+
+  // enable all outputs (active-low)
+  digitalWrite(shiftOutputEnable, LOW);
+  
+  // clear registers (active-low)
+  digitalWrite(shiftClear, LOW);
+  delay(250);
+  digitalWrite(shiftClear, HIGH);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(green_led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(green_led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  for (int i = 0; i <= kNumLeds; i++) {
+    enableLightsUpTo(i);
+    delay(500);
+  }
 }
