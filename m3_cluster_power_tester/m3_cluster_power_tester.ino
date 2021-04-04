8a4f86cbcd93fcd7d7815868f36eb6506cab4de3
Ethan Chen <intervigil@gmail.com>
2021-04-03 20:44:02 -0700

    power_tester: Fix constant naming

--- m3_cluster_power_tester/m3_cluster_power_tester.ino
+++ m3_cluster_power_tester/m3_cluster_power_tester.ino
@@ -2,36 +2,36 @@ int internalLed = 13;
 
 const int kNumLeds = 8;
 
-const int shiftSerialIn = 2;
-const int shiftClock = 3;
-const int shiftRegClock = 4;
-const int shiftClear = 19;
-const int shiftOutputEnable = 18;
+const int kShiftSerialInPin = 2;
+const int kShiftClockPin = 3;
+const int kShiftRegClockPin = 4;
+const int kShiftClearPin = 19;
+const int kShiftOutputEnablePin = 18;
 
 void enableLightsUpTo(int idx) {
   if (idx < 0 || idx > kNumLeds) {
     return;
   }
-  digitalWrite(shiftRegClock, LOW);
-  shiftOut(shiftSerialIn, shiftClock, MSBFIRST, (1 << idx) - 1);
-  digitalWrite(shiftRegClock, HIGH);
+  digitalWrite(kShiftRegClockPin, LOW);
+  shiftOut(kShiftSerialInPin, kShiftClockPin, MSBFIRST, (1 << idx) - 1);
+  digitalWrite(kShiftRegClockPin, HIGH);
 }
 
 // the setup routine runs once when you press reset:
 void setup() {
-  pinMode(shiftSerialIn, OUTPUT);
-  pinMode(shiftClock, OUTPUT);
-  pinMode(shiftRegClock, OUTPUT);
-  pinMode(shiftClear, OUTPUT);
-  pinMode(shiftOutputEnable, OUTPUT);
+  pinMode(kShiftSerialInPin, OUTPUT);
+  pinMode(kShiftClockPin, OUTPUT);
+  pinMode(kShiftRegClockPin, OUTPUT);
+  pinMode(kShiftClearPin, OUTPUT);
+  pinMode(kShiftOutputEnablePin, OUTPUT);
 
   // enable all outputs (active-low)
-  digitalWrite(shiftOutputEnable, LOW);
-  
+  digitalWrite(kShiftOutputEnablePin, LOW);
+
   // clear registers (active-low)
-  digitalWrite(shiftClear, LOW);
+  digitalWrite(kShiftClearPin, LOW);
   delay(250);
-  digitalWrite(shiftClear, HIGH);
+  digitalWrite(kShiftClearPin, HIGH);
 }
 
 // the loop routine runs over and over again forever:
