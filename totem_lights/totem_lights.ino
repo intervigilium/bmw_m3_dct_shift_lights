fa6452af81bc3662ca7eb4eada20d6613c27918a
Ethan Chen <intervigil@gmail.com>
2021-10-10 14:15:00 -0700

    totem_lights: neopixel sample code import

--- totem_lights/totem_lights.ino
+++ totem_lights/totem_lights.ino
@@ -1,11 +1,27 @@
 #include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+#define PIN        6
+#define NUMPIXELS 16
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
 
 void setup() {
-  // put your setup code here, to run once:
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
 
+  pixels.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  pixels.clear();
 
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.show();
+    delay(DELAYVAL);
+  }
 }
